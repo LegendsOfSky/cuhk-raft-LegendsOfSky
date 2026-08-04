79fe2caae688a53979acf61cf8f25ea4a94bb01c
LegendsOfSky <legendsofskywork@gmail.com>
2026-08-04 12:40:10 +0800

    formatting

--- yourCode/main.go
+++ yourCode/main.go
@@ -4,19 +4,20 @@ import (
 	"context"
 	"cuhk/asgn/raft"
 	"fmt"
-	"google.golang.org/grpc"
 	"log"
 	"net"
 	"os"
 	"strconv"
 	"strings"
 	"time"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
 	ports := os.Args[2]
-	myport, _ := strconv.Atoi(os.Args[1])
-	nodeID, _ := strconv.Atoi(os.Args[3])
+	myPort, _ := strconv.Atoi(os.Args[1])
+	nodeId, _ := strconv.Atoi(os.Args[3])
 	heartBeatInterval, _ := strconv.Atoi(os.Args[4])
 	electionTimeout, _ := strconv.Atoi(os.Args[5])
 
@@ -25,15 +26,15 @@ func main() {
 	// A map where
 	// 		the key is the node id
 	//		the value is the {hostname:port}
-	nodeidPortMap := make(map[int]int)
+	nodeIdPortMap := make(map[int]int)
 	for i, portStr := range portStrings {
 		port, _ := strconv.Atoi(portStr)
-		nodeidPortMap[i] = port
+		nodeIdPortMap[i] = port
 	}
 
 	// Create and start the Raft Node.
-	_, err := NewRaftNode(myport, nodeidPortMap,
-		nodeID, heartBeatInterval, electionTimeout)
+	_, err := NewRaftNode(myPort, nodeIdPortMap,
+		nodeId, heartBeatInterval, electionTimeout)
 
 	if err != nil {
 		log.Fatalln("Failed to create raft node:", err)
@@ -49,60 +50,73 @@ type raftNode struct {
 
 }
 
-// Desc:
-// NewRaftNode creates a new RaftNode. This function should return only when
-// all nodes have joined the ring, and should return a non-nil error if this node
-// could not be started in spite of dialing any other nodes.
+// NewRaftNode Summary
 //
-// Params:
-// myport: the port of this new node. We use tcp in this project.
-//			   	Note: Please listen to this port rather than nodeidPortMap[nodeId]
-// nodeidPortMap: a map from all node IDs to their ports.
+// creates a new RaftNode.
+//
+// # Parameters
+//
+// myPort: the port of this new node. We use tcp in this project.
+// (Note: Please listen to this port rather than nodeidPortMap[nodeId])
+//
+// nodeIdPortMap: a map from all node IDs to their ports.
+//
 // nodeId: the id of this node
+//
 // heartBeatInterval: the Heart Beat Interval when this node becomes leader. In millisecond.
+//
 // electionTimeout: The election timeout for this node. In millisecond.
-func NewRaftNode(myport int, nodeidPortMap map[int]int, nodeId, heartBeatInterval,
-	electionTimeout int) (raft.RaftNodeServer, error) {
+//
+// # Returns
+//
+// This function should return only when all nodes have joined the ring, and should return a non-nil error if this node
+// could not be started in spite of dialing any other nodes.
+func NewRaftNode(
+	myPort int, nodeIdPortMap map[int]int, nodeId, heartBeatInterval, electionTimeout int) (
+	raft.RaftNodeServer, error) {
 	// TODO: Implement this!
 
-	//remove myself in the hostmap
-	delete(nodeidPortMap, nodeId)
+	delete(nodeIdPortMap, nodeId) // remove myself in the host-map
 
-	//a map for {node id, gRPCClient}
-	hostConnectionMap := make(map[int32]raft.RaftNodeClient)
+	hostConnectionMap := make(map[int32]raft.RaftNodeClient) // a map for {node id, gRPCClient}
 
-	rn := raftNode{
+	newRaftNode := raftNode{
 		log: nil,
 	}
 
-	l, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", myport))
+	listener, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", myPort))
 
 	if err != nil {
 		log.Println("Fail to listen port", err)
 		os.Exit(1)
 	}
 
-	s := grpc.NewServer()
-	raft.RegisterRaftNodeServer(s, &rn)
+	server := grpc.NewServer()
+	raft.RegisterRaftNodeServer(server, &newRaftNode)
 
-	log.Printf("Start listening to port: %d", myport)
-	go s.Serve(l)
+	log.Printf("Start listening to port: %d\n", myPort)
+	go func() {
+		err := server.Serve(listener)
+		if err != nil {
+			log.Printf("Server listening to port %d failed.\n", myPort)
+		}
+	}()
 
-	//Try to connect nodes
-	for tmpHostId, hostPorts := range nodeidPortMap {
-		hostId := int32(tmpHostId)
+	// Try connecting nodes
+	for hostId, hostPorts := range nodeIdPortMap {
+		currentHostId := int32(hostId)
 		numTry := 0
 		for {
 			numTry++
 
 			conn, err := grpc.Dial(fmt.Sprintf("127.0.0.1:%d", hostPorts), grpc.WithInsecure(), grpc.WithBlock())
-			//defer conn.Close()
+			// defer conn.Close()
 			client := raft.NewRaftNodeClient(conn)
 			if err != nil {
 				log.Println("Fail to connect other nodes. ", err)
 				time.Sleep(1 * time.Second)
 			} else {
-				hostConnectionMap[hostId] = client
+				hostConnectionMap[currentHostId] = client
 				break
 			}
 		}
@@ -111,23 +125,20 @@ func NewRaftNode(myport int, nodeidPortMap map[int]int, nodeId, heartBeatInterva
 
 	//TODO: kick off leader election here !
 
-	return &rn, nil
+	return &newRaftNode, nil
 }
 
-// Desc:
-// Propose initializes proposing a new operation, and replies with the
-// result of committing this operation. Propose should not return until
-// this operation has been committed, or this node is not leader now.
+// Propose Summary
 //
-// If the we put a new <k, v> pair or deleted an existing <k, v> pair
-// successfully, it should return OK; If it tries to delete an non-existing
-// key, a KeyNotFound should be returned; If this node is not leader now,
-// it should return WrongNode as well as the currentLeader id.
+// initializes proposing a new operation, and replies with the result of committing this operation. Propose
+// should not return until this operation has been committed, or this node is not leader now.
 //
-// Params:
-// args: the operation to propose
-// reply: as specified in Desc
-func (rn *raftNode) Propose(ctx context.Context, args *raft.ProposeArgs) (*raft.ProposeReply, error) {
+// If we put a new <k, v> pair or deleted an existing <k, v> pair successfully, it should return OK;
+//
+// If it tries to delete a non-existing key, a KeyNotFound should be returned;
+//
+// If this node is not leader now, it should return WrongNode as well as the currentLeader id.
+func (rn *raftNode) Propose(ctx context.Context, operationToBeProposed *raft.ProposeArgs) (*raft.ProposeReply, error) {
 	// TODO: Implement this!
 	log.Printf("Receive propose from client")
 	var ret raft.ProposeReply
@@ -135,72 +146,76 @@ func (rn *raftNode) Propose(ctx context.Context, args *raft.ProposeArgs) (*raft.
 	return &ret, nil
 }
 
-// Desc:GetValue
-// GetValue looks up the value for a key, and replies with the value or with
-// the Status KeyNotFound.
+// GetValue Summary
 //
-// Params:
-// args: the key to check
-// reply: the value and status for this lookup of the given key
-func (rn *raftNode) GetValue(ctx context.Context, args *raft.GetValueArgs) (*raft.GetValueReply, error) {
+// looks up the value for a key, and replies with the value or with the Status KeyNotFound.
+func (rn *raftNode) GetValue(ctx context.Context, keyOfTheValue *raft.GetValueArgs) (*raft.GetValueReply, error) {
 	// TODO: Implement this!
 	var ret raft.GetValueReply
 	return &ret, nil
 }
 
-// Desc:
+// RequestVote Summary
+//
 // Receive a RecvRequestVote message from another Raft Node. Check the paper for more details.
 //
-// Params:
-// args: the RequestVote Message, you must include From(src node id) and To(dst node id) when
-// you call this API
-// reply: the RequestVote Reply Message
-func (rn *raftNode) RequestVote(ctx context.Context, args *raft.RequestVoteArgs) (*raft.RequestVoteReply, error) {
+// # Parameters
+//
+// voteRequestArgs: The RequestVote Message. From (src node id) and To (dst node id) must be included.
+//
+// TODO: rename to HandleVoteRequest
+func (rn *raftNode) RequestVote(ctx context.Context, voteRequestArgs *raft.RequestVoteArgs) (
+	*raft.RequestVoteReply, error) {
 	// TODO: Implement this!
 	var reply raft.RequestVoteReply
 	return &reply, nil
 }
 
-// Desc:
+// AppendEntries Summary
+//
 // Receive a RecvAppendEntries message from another Raft Node. Check the paper for more details.
 //
-// Params:
-// args: the AppendEntries Message, you must include From(src node id) and To(dst node id) when
-// you call this API
-// reply: the AppendEntries Reply Message
-func (rn *raftNode) AppendEntries(ctx context.Context, args *raft.AppendEntriesArgs) (*raft.AppendEntriesReply, error) {
+// # Parameters
+//
+// appendEntriesArgs: The AppendEntries Message. From (src node id) and To (dst node id) must be included.
+func (rn *raftNode) AppendEntries(ctx context.Context, appendEntriesArgs *raft.AppendEntriesArgs) (
+	*raft.AppendEntriesReply, error) {
 	// TODO: Implement this
 	var reply raft.AppendEntriesReply
 	return &reply, nil
 }
 
-// Desc:
-// Set electionTimeOut as args.Timeout milliseconds.
-// You also need to stop current ticker and reset it to fire every args.Timeout milliseconds.
+// SetElectionTimeout Summary
 //
-// Params:
-// args: the heartbeat duration
-// reply: no use
-func (rn *raftNode) SetElectionTimeout(ctx context.Context, args *raft.SetElectionTimeoutArgs) (*raft.SetElectionTimeoutReply, error) {
+// Set electionTimeOut with args.Timeout in milliseconds (will stop and reset the timer).
+//
+// # Returns
+//
+// Not in use.
+func (rn *raftNode) SetElectionTimeout(
+	ctx context.Context, electionTimeoutInterval *raft.SetElectionTimeoutArgs) (*raft.SetElectionTimeoutReply, error) {
 	// TODO: Implement this!
 	var reply raft.SetElectionTimeoutReply
 	return &reply, nil
 }
 
-// Desc:
-// Set heartBeatInterval as args.Interval milliseconds.
-// You also need to stop current ticker and reset it to fire every args.Interval milliseconds.
+// SetHeartBeatInterval Summary
 //
-// Params:
-// args: the heartbeat duration
-// reply: no use
-func (rn *raftNode) SetHeartBeatInterval(ctx context.Context, args *raft.SetHeartBeatIntervalArgs) (*raft.SetHeartBeatIntervalReply, error) {
+// Set heartBeatInterval with args.Interval in milliseconds (will stop and reset the timer).
+//
+// # Returns
+//
+// Not in use.
+func (rn *raftNode) SetHeartBeatInterval(ctx context.Context, hearBeatInterval *raft.SetHeartBeatIntervalArgs) (
+	*raft.SetHeartBeatIntervalReply, error) {
 	// TODO: Implement this!
 	var reply raft.SetHeartBeatIntervalReply
 	return &reply, nil
 }
 
-//NO NEED TO TOUCH THIS FUNCTION
+// CheckEvents WARNING
+//
+// NO NEED TO TOUCH THIS FUNCTION
 func (rn *raftNode) CheckEvents(context.Context, *raft.CheckEventsArgs) (*raft.CheckEventsReply, error) {
 	return nil, nil
 }
